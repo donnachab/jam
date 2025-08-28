1d4936fe0b76c428df727105b851d0db4ef87969
donnachab <61333383+donnachab@users.noreply.github.com>
2025-08-28 10:46:23 +0100

    Update firebase-config.js

--- public/js/firebase-config.js
+++ public/js/firebase-config.js
@@ -4,14 +4,14 @@ import { getFirestore } from "https://www.gstatic.com/firebasejs/9.6.1/firebase-
 
 // --- Your Firebase Configuration ---
 // IMPORTANT: Replace this with your actual Firebase project configuration
-  const firebaseConfig = {
-    apiKey: "AIzaSyC4SnqaOMQWmEFulkN8zZALZsqJLK7hOh0",
-    authDomain: "galway-jam-circle-live.firebaseapp.com",
-    projectId: "galway-jam-circle-live",
-    storageBucket: "galway-jam-circle-live.firebasestorage.app",
-    messagingSenderId: "140452021164",
-    appId: "1:140452021164:web:049a190be3ba0b6c9a3009"
-  };
+const firebaseConfig = {
+  apiKey: "AIzaSyC4SnqaOMQWmEFulkN8zZALZsqJLK7hOh0",
+  authDomain: "galway-jam-circle-live.firebaseapp.com",
+  projectId: "galway-jam-circle-live",
+  storageBucket: "galway-jam-circle-live.firebasestorage.app",
+  messagingSenderId: "140452021164",
+  appId: "1:140452021164:web:049a190be3ba0b6c9a3009"
+};
 
 
 // Initialize Firebase
@@ -22,4 +22,5 @@ console.log("✅ Firebase initialized successfully.");
 
 // Export the database instance to be used in other modules
 export { db };
+
 
