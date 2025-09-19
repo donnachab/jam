b00507a54609179f92348c7ffaf4319758d9292c
donnachab <donnachab@gmail.com>
2025-09-19 14:27:42 +0000

    Fix: Center community images within their container
    This commit adds a nested flexbox container to explicitly center the images within their fixed aspect-ratio container in the community slideshow.

--- public/js/community.js
+++ public/js/community.js
@@ -41,7 +41,9 @@ export function renderCommunity(items) {
             slide.innerHTML = `
                 <div class="bg-white overflow-hidden border border-gray-200">
                     <div class="relative bg-gray-200" style="padding-bottom: 56.25%;">
-                        <img src="${item.imageUrl}" alt="${item.description.substring(0, 50)}" class="absolute w-full h-full object-contain">
+                        <div class="absolute top-0 left-0 w-full h-full flex items-center justify-center">
+                            <img src="${item.imageUrl}" alt="${item.description.substring(0, 50)}" class="max-w-full max-h-full object-contain">
+                        </div>
                         <div class="slide-admin-controls admin-controls-inline space-x-2">
                             <button data-id="${item.id}" class="edit-community-btn bg-blue-500 text-white p-2 rounded-full hover:bg-blue-600 shadow-md"><svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" viewBox="0 0 20 20" fill="currentColor"><path d="M17.414 2.586a2 2 0 00-2.828 0L7 10.172V13h2.828l7.586-7.586a2 2 0 000-2.828z" /><path fill-rule="evenodd" d="M2 6a2 2 0 012-2h4a1 1 0 010 2H4v10h10v-4a1 1 0 112 0v4a2 2 0 01-2 2H4a2 2 0 01-2-2V6z" clip-rule="evenodd" /></svg></button>
                             <button data-id="${item.id}" class="delete-community-btn bg-red-500 text-white p-2 rounded-full hover:bg-red-600 shadow-md"><svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" viewBox="0 0 20 20" fill="currentColor"><path fill-rule="evenodd" d="M9 2a1 1 0 00-.894.553L7.382 4H4a1 1 0 000 2v10a2 2 0 002 2h8a2 2 0 002-2V6a1 1 0 100-2h-3.382l-.724-1.447A1 1 0 0011 2H9zM7 8a1 1 0 012 0v6a1 1 0 11-2 0V8zm4 0a1 1 0 012 0v6a1 1 0 11-2 0V8z" clip-rule="evenodd" /></svg></button>
