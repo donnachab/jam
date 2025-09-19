11993b9b3a1e5a5b16cefbf835d953d54c5aebef
donnachab <donnachab@gmail.com>
2025-09-19 14:45:10 +0000

    Feat: Apply new design system based on Galway City Council styles
    This commit implements a major redesign of the website based on the styles of the Galway City Council.
    
    - A new design system has been implemented using CSS variables and Tailwind CSS.
    - All components have been updated to use the new fonts, colors, and spacing.
    - This creates a more professional and consistent look and feel for the site.

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,11 +3,24 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        primary: 'var(--primary-color)',
-        accent: 'var(--accent-color)',
-        background: 'var(--background-color)',
-        'text-dark': 'var(--text-dark)',
-        'text-light': 'var(--text-light)',
+        primary: 'var(--color-accent)',
+        accent: 'var(--color-accent-2)',
+        green: 'var(--color-green)',
+        red: 'var(--color-red)',
+        orange: 'var(--color-orange)',
+        yellow: 'var(--color-yellow)',
+        blue: 'var(--color-blue)',
+        white: 'var(--color-white)',
+        'grey-lightest': 'var(--color-grey-lightest)',
+        'grey-lighter': 'var(--color-grey-lighter)',
+        'grey-light': 'var(--color-grey-light)',
+        'grey-medium': 'var(--color-grey-medium)',
+        'grey-dark': 'var(--color-grey-dark)',
+        black: 'var(--color-black)',
+      },
+      fontFamily: {
+        primary: ['roboto', 'oxygen', 'ubuntu', 'cantarell', 'Open Sans', 'Helvetica Neue', 'sans-serif'],
+        secondary: ['georgia', 'Times New Roman', 'times', 'serif'],
       }
     },
   },
